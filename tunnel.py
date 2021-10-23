2e1981de267249df40ae1839733b5a7c1cd10ec2
Mercy.ThuyLe <62128227+WhiTExB3AR@users.noreply.github.com>
2021-10-24 00:12:12 +0700

    Update tunnel.py

--- tunnel.py
+++ tunnel.py
@@ -1,7 +1,10 @@
 import nest_asyncio
-from pyngrok import ngrok
+from pyngrok import conf, ngrok
 import time
 
+# Setting an auth token allows us to open multiple
+# tunnels at the same time
+ngrok.set_auth_token("1z1di3c8qZMo13cDdSmPlPhuxJP_72JpP4W3UNPXvN4ExUZP2")
 ngrok_tunnel = ngrok.connect(8097)
 print('Public URL:', ngrok_tunnel.public_url)
 nest_asyncio.apply()
