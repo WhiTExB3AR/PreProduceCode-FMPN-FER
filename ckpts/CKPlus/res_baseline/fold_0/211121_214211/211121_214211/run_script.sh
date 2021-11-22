6b6c0baa8a7e9e5efd654ac9eafcd121be89f405
Mercy.ThuyLe <62128227+WhiTExB3AR@users.noreply.github.com>
2021-11-22 09:51:23 +0700

    Both CKPlus and JAFFE pre-train

---
+++ ckpts/CKPlus/res_baseline/fold_0/211121_214211/211121_214211/run_script.sh
@@ -0,0 +1,1 @@
+[train][211121_214211]python main.py --mode train --data_root datasets/CKPlus --train_csv train_both_0.csv --batch_size 16 --print_losses_freq 4 --use_data_augment --visdom_env main_baseline_ckp_0 --niter 100 --niter_decay 100 --gpu_ids 0 --model res_baseline --solver resface --img_nc 1 --visdom_port 80 --visdom_ip http://fcaf-2402-800-63a7-932b-e501-ea20-8deb-59d4.ngrok.io
