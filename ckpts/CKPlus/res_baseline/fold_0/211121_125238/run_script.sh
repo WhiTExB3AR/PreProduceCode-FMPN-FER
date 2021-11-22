f54ece58f5646d40e73706fa2a8621bb6d71bab4
Mercy.ThuyLe <62128227+WhiTExB3AR@users.noreply.github.com>
2021-11-22 09:36:29 +0700

    CKPlus pre-train fold 0

---
+++ ckpts/CKPlus/res_baseline/fold_0/211121_125238/run_script.sh
@@ -0,0 +1,1 @@
+[train][211121_125238]python main.py --mode train --data_root datasets/CKPlus --train_csv trainCK_ids_0.csv --print_losses_freq 4 --use_data_augment --visdom_env main_baseline_ckp_0 --niter 30 --niter_decay 30 --gpu_ids 0 --model res_baseline --solver resface --img_nc 1 --visdom_port 80 --visdom_ip http://ec33-2405-4800-52a7-1c4f-4de8-6920-8e5-73f8.ngrok.io
